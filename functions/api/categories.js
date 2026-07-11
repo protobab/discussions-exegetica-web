78b2855c0d77b5a752cc6dca203b0bed5b82e5b9
protobab <protobab@proton.me>
2026-07-11 13:36:41 +0100

    Delete functions directory

--- functions/api/categories.js
+++
@@ -1,5 +0,0 @@
-// functions/api/categories.js
-export async function onRequestGet({ env }) {
-  const { results } = await env.DB.prepare(`SELECT slug, label, icon, description FROM categories ORDER BY sort_order`).all()
-  return new Response(JSON.stringify({ categories: results }), { headers: { 'Content-Type':'application/json', 'Access-Control-Allow-Origin':'*', 'Cache-Control':'public,max-age=86400' }})
-}
